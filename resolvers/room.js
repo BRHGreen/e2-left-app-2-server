5f77fd0eb5d3c634e49cbb735d6c681987dd812c
BRHGreen <benedictgreen@gmail.com>
2018-08-11 22:56:24 +0100

    ordering rooms

--- resolvers/room.js
+++ resolvers/room.js
@@ -1,9 +1,12 @@
 // import formatErrors from '../formatErrors';
 // import requiresAuth from '../permissions';
+import sequelize from 'sequelize'
 
 export default {
   
   Query: {
-    getRooms: (parent, args, { models }) => models.Room.findAll()
+    getRooms: (parent, args, { models }) => models.Room.findAll({
+      order: sequelize.col('roomNumber')
+    })
   },
 };
